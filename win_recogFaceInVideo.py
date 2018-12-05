a35ee0ae5cf401f0eb4960076ecbc300d7c6e2b8
view1994 <379144671@qq.com>
2018-12-06 03:16:28 +0800

    识别视频中人脸窗口添加功能： 添加打开摄像头功能，摄像头抓取的画面经过人脸识别标记后居中显示在界面标签中，且'打开摄像头'按钮添加按下保持和再次点击弹起的功能，实现一个按钮在'打开摄像头'和'关闭摄像头'两种功能中切换，且在退出窗口前释放摄像头后再退回到主菜单。

--- win_recogFaceInVideo.py
+++ win_recogFaceInVideo.py
@@ -18,10 +18,12 @@ class Win_RecogFaceInVideo(QWidget, Ui_win_recogFaceInVideo):
     def __init__(self, parent=None ):
         super(Win_RecogFaceInVideo, self).__init__(parent)
         self.setupUi(self)
+        self.button_openCamera.setCheckable(True)
+        self.button_openCamera.setAutoExclusive(True)
         self.button_openCamera.clicked.connect(self.recog_in_camera)
         self.button_openVideoFile.clicked.connect(self.open_video_file)
         self.commandLinkButton.clicked.connect(self.back2main)
-
+        #self.button_openCamera.triggered.connect(self.close_camera())
     windowList = []
     ###### 重写关闭事件，回到第一界面
     def closeEvent(self, event):
@@ -31,15 +33,17 @@ class Win_RecogFaceInVideo(QWidget, Ui_win_recogFaceInVideo):
         mainWin.show()
         event.accept()
     def back2main(self):
-        #if self.video_capture:
         print('video_capture.release')
-         #   self.video_capture.release()
+        if self.button_openCamera.isChecked():
+            self.close_camera()
         cv2.destroyAllWindows()
         self.close()
 
     video_capture = []
     def open_video_file(self):
+        self.button_openCamera.setEnabled(False)
         fname, _ = myLib.loadFile(self,'video')
+        self.textEdit.setText(fname)
         self.video_capture = cv2.VideoCapture(fname)
         self.known_face_encodings, self.known_face_names = face_recog.get_featuresArray_fromDB()
         self.timer_camera = QTimer()
@@ -47,12 +51,23 @@ class Win_RecogFaceInVideo(QWidget, Ui_win_recogFaceInVideo):
         self.timer_camera.timeout.connect(self.play1pic)  # 连接槽函数
 
     def recog_in_camera(self):
-        self.video_capture = cv2.VideoCapture(0)
-        self.known_face_encodings, self.known_face_names = face_recog.get_featuresArray_fromDB()
-        self.timer_camera = QTimer()
-        self.timer_camera.start(10)  # 1000ms == 1s
-        self.timer_camera.timeout.connect(self.play1pic)  # 连接槽函数
-
+        if  self.button_openCamera.isChecked():
+            self.button_openVideoFile.setEnabled(False)
+            self.button_openCamera.setText('关闭摄像头')
+            self.video_capture = cv2.VideoCapture(0)
+            self.known_face_encodings, self.known_face_names = face_recog.get_featuresArray_fromDB()
+            self.timer_camera = QTimer()
+            self.timer_camera.start(10)  # 1000ms == 1s
+            self.timer_camera.timeout.connect(self.play1pic)  # 连接槽函数
+        else:
+            self.close_camera()
+    def close_camera(self):
+        self.video_capture.release()
+        self.timer_camera.stop()
+        self.button_openCamera.setText('打开摄像头')
+        self.button_openVideoFile.setEnabled(True)
+        self.label.setText('请选择视频文件或打开摄像头')
+        #self.label.setPixmap(None)
     def play1pic(self):
         ret, frame = self.video_capture.read()
         if ret:
@@ -64,6 +79,7 @@ class Win_RecogFaceInVideo(QWidget, Ui_win_recogFaceInVideo):
         else:
             self.video_capture.release()
             self.timer_camera.stop()
+            self.button_openCamera.setEnabled(True)
     def recog_in_v(self,bgr_frame):
         # 改变摄像头图像的大小，图像小，所做的计算就少
         small_frame = cv2.resize(bgr_frame, (0, 0), fx=0.25, fy=0.25)
@@ -95,62 +111,6 @@ class Win_RecogFaceInVideo(QWidget, Ui_win_recogFaceInVideo):
             font = cv2.FONT_HERSHEY_DUPLEX
             cv2.putText(bgr_frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
         return bgr_frame
-
-    def recog_in_camera1(self,fname = 0):
-        video_capture = cv2.VideoCapture(fname)
-        # Create arrays of known face encodings and their names
-        # 脸部特征数据的集合 # 人物名称的集合
-        known_face_encodings , known_face_names = face_recog.get_featuresArray_fromDB()
-        face_locations = []
-        face_encodings = []
-        face_names = []
-        process_this_frame = True
-        # 读取摄像头画面
-        ret, frame = video_capture.read()
-        while ret:
-            # 改变摄像头图像的大小，图像小，所做的计算就少
-            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-            # opencv的图像是BGR格式的，而我们需要是的RGB格式的，因此需要进行一个转换。
-            rgb_small_frame = small_frame[:, :, ::-1]
-            # Only process every other frame of video to save time
-            if process_this_frame:
-                # 根据encoding来判断是不是同一个人，是就输出true，不是为flase
-                face_locations = face_recognition.face_locations(rgb_small_frame)
-                face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-                face_names = []
-                for face_encoding in face_encodings:
-                    # 默认为unknown
-                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                    name = "Unknown"
-                    # If a match was found in known_face_encodings, just use the first one.
-                    if True in matches:
-                        first_match_index = matches.index(True)
-                        name = known_face_names[first_match_index]
-                    face_names.append(name)
-            process_this_frame = not process_this_frame
-            # 将捕捉到的人脸显示出来
-            for (top, right, bottom, left), name in zip(face_locations, face_names):
-                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
-                # 矩形框
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-                #加上标签
-                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-                font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-            # Display
-            cv2.imshow('monitor', frame)
-            # 读取摄像头画面
-            ret, frame = video_capture.read()
-            # 按Q退出
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        print('go out of while')
-        video_capture.release()
-        cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
